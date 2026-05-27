6a86d435308fbe7f469899e01bc10b5772ceafb1
Connor <wxj995@gmail.com>
2026-05-27 09:18:16 -0700

    add hellosystemc

---
+++ HelloSystemC/include/main.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+
+void PrintMain(){
+    std::cout << "Call Main." << std::endl;
+}
---
+++ HelloSystemC/source/main.cpp
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "common.h"
+#include <systemc.h>
+
+using namespace sc_core;
+    SC_MODULE(Hello) {
+        SC_CTOR(Hello) {
+            SC_THREAD(run);
+    }
+
+    void run() {
+        std::cout << "Call Hello SystemC @ " << sc_time_stamp() << "\n";
+        wait(10, SC_NS);
+        std::cout << "After 10 ns @ " << sc_time_stamp() << "\n";
+        sc_stop();
+    }
+};
+
+int sc_main(int argc, char* argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    PrintCommon();
+    PrintMain();
+
+    Hello h("h");
+    sc_start();
+
+    return 0;
+}
+
+
